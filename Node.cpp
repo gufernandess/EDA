9f4a2ba3ab35b269e53cc860758c63979399c2dc
gufernandess <gustavofernandescc@gmail.com>
2023-03-21 15:58:43 -0300

    Nó básico

---
+++ Node.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Gustavo Fernandes on 21/03/2023.
+//
+
+#include "Node.h"
---
+++ Node.h
@@ -0,0 +1,20 @@
+//
+// Created by Gustavo Fernandes on 21/03/2023.
+//
+
+#ifndef EDA_NODE_H
+#define EDA_NODE_H
+
+struct Node {
+    int key;
+    Node* left;
+    Node* right;
+
+    Node(int key, Node* left, Node* right) {
+        this->key;
+        this->left;
+        this->right;
+    }
+};
+
+#endif //EDA_NODE_H

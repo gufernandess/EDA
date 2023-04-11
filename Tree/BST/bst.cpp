4e87cdbdaffff990304da7977a20c64bd777ce10
gufernandess <gustavofernandescc@gmail.com>
2023-04-10 21:52:02 -0300

    BST´s iterativas

--- Tree/BST/bst.cpp
+++ Tree/BST/bst.cpp
@@ -77,6 +77,70 @@ Node *BST::sucessor(Node *node, Node *root) {
     }
 }
 
+Node *BST::predecessor(Node *key, Node* root) {
+    if(key == nullptr || root == nullptr) {
+        return nullptr;
+    }
+
+    else if(key->left != nullptr) {
+        return maximum(key->left);
+    }
+
+    else {
+        return ancestral_predecessor(key, root);
+    }
+}
+
+Node *BST::remove(int key, Node* node) {
+    if(node == nullptr) {
+        return nullptr;
+    }
+
+    if(node->key > key) {
+        node->left = remove(key, node->left);
+    }
+
+    else if(node->key < key) {
+        node->right = remove(key, node->right);
+    }
+
+    else {
+        node = removeRoot(node);
+    }
+
+    return node;
+}
+
+Node *BST::removeRoot(Node *node) {
+    if(node->left == nullptr && node->right == nullptr) {
+        delete node;
+        return nullptr;
+    }
+
+    else if(node->left == nullptr) {
+        Node *temp = node->right;
+        delete node;
+        return temp;
+    }
+
+    else if(node->right == nullptr) {
+        Node *temp = node->left;
+        delete node;
+        return temp;
+    }
+
+    else {
+        Node *temp = minimum(node->right);
+        node->key = temp->key;
+        node->right = remove(temp->key, node->right);
+        return node;
+    }
+}
+
+void BST::remove(int key) {
+    root = remove(key, root);
+}
+
 int BST::successor(int key) {
     Node *temp = search(root, key);
 
@@ -89,6 +153,25 @@ int BST::successor(int key) {
 
         else {
             throw "Não existe sucessor";
+        }
+    }
+
+    else throw "Não existe nó com essa chave";
+}
+
+
+int BST::predecessor(int key) {
+    Node *temp = search(root, key);
+
+    if(temp != nullptr) {
+        Node *pred = predecessor(temp, root);
+
+        if(pred != nullptr) {
+            return pred->key;
+        }
+
+        else {
+            throw "Não existe predecessor";
         }
     }
 
